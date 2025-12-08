4d024bbf2572a56835e1fab56c4a0c4b0d78a7e4
Gary Horan <gary.horan1@det.nsw.edu.au>
2025-12-08 15:16:53 +1100

    more changes

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+# This program simply is part of 9CT Task 2 and prints the words "hello world" on the terminal
+print("----Start of Output ---------------------------")
+print("Hello World")      
+print("----End of Output ---------------------------")
